3ef586bebaa34aec0fb7aabcc2b4852388ad4929
Ben Blowers <benblowers@outlook.com>
2016-08-19 18:06:43 +0100

    App index

---
+++ heist-api/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+var environment = app.get('env');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var port = process.env.PORT || 3000;
+var routes = require('./config/routes');
+var databaseUri = require('./config/db')(environment);
+
+mongoose.connect(databaseUri);
+
+if('test' !== environment) {
+  app.use(require('morgan')('dev'));
+}
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+app.listen(port, function(){
+  console.log("Express is listening to port " + port);
+});
+
+module.exports = app;
