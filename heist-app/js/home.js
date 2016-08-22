c157478f18b19e6f72c0a4d187b0a796281fd0b2
Cameron Perrin <perrin.cameron@gmail.com>
2016-08-22 15:39:19 +0100

    centered and added a fading image for the homepage

--- heist-app/js/home.js
+++ heist-app/js/home.js
@@ -1,6 +1,8 @@
 $circle = $(".circle")
 $line1 = $(".line1")
 $line2 = $(".line2")
+var promoNum = 1
+
 
 $circle.hide();
 $line1.hide();
@@ -8,3 +10,16 @@ $line2.hide();
 $line1.animate({width:'toggle'},1500);
 $line2.animate({width:'toggle'},1500);
 $circle.fadeIn(1500);
+
+setInterval(function() {
+  if(promoNum < 4){
+    promoNum++;
+  }else{
+    promoNum = 1;
+  }
+  $(".background").fadeOut("slow")
+  setTimeout(function(){
+    $(".background").css("background-image", "url('../images/promo"+promoNum+".jpg')");
+    $(".background").fadeIn("slow");
+  }, 500);
+}, 3000);
