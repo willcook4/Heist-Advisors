f43df522fe4e1931f4dbf91147779cf5638bc640
Will Cook <nzwillcook@gmail.com>
2016-08-25 16:30:55 +0100

    Setting up for deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 var routes = require('./config/routes');
 var databaseUri = require('./config/db')(environment);
 
@@ -20,7 +20,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static("public"));
 app.use('/api', routes);
 
 app.listen(port, function(){
--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/heists-api-test",
   development: "mongodb://localhost/heists-api",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/heists-api"
+  production: process.env.MONGODB_URI || "mongodb://localhost/heists-api"
 }
 
 module.exports = function(env) {
